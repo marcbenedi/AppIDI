ec4149c4b4e636797d60427ab91ae29d20d9230b
Marc Benedí <marcbenedi.sanmillan@gmail.com>
2016-12-28 16:03:58 +0100

    Fragment Structure Created for first screen of A block

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/BooksSortedByCategoryFragment.java
@@ -0,0 +1,22 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BooksSortedByCategoryFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.books_sorted_by_category_layout,container,false);
+        return v;
+    }
+}
